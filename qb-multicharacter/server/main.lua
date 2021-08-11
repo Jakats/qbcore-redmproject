e1c50b24e7584d05952c3d7e655df42af7ef6bb2
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-08-11 13:57:44 +0200

    Change for creating character and houses and apartments config

--- qb-multicharacter/server/main.lua
+++ qb-multicharacter/server/main.lua
@@ -1,7 +1,6 @@
 RegisterServerEvent('qb-multicharacter:server:disconnect')
 AddEventHandler('qb-multicharacter:server:disconnect', function()
     local src = source
-
     DropPlayer(src, "You have disconnected from QBCore RedM")
 end)
 
@@ -11,55 +10,31 @@ AddEventHandler('qb-multicharacter:server:loadUserData', function(cData)
     if QBCore.Player.Login(src, cData.citizenid) then
         print('^2[qb-core]^7 '..GetPlayerName(src)..' (Citizen ID: '..cData.citizenid..') has succesfully loaded!')
         QBCore.Commands.Refresh(src)
-        loadHouseData()
-		--TriggerEvent('QBCore:Server:OnPlayerLoaded')-
-        --TriggerClientEvent('QBCore:Client:OnPlayerLoaded', src)
-        
-        TriggerClientEvent('apartments:client:setupSpawnUI', src, cData)
+
+        TriggerClientEvent("qb-multicharacter:client:closeNUI", src)
+        TriggerClientEvent('qb-spawn:client:setupSpawnUI', src, cData, false)
         TriggerEvent("qb-log:server:CreateLog", "joinleave", "Loaded", "green", "**".. GetPlayerName(src) .. "** ("..cData.citizenid.." | "..src..") loaded..")
 	end
 end)
 
 RegisterServerEvent('qb-multicharacter:server:createCharacter')
-AddEventHandler('qb-multicharacter:server:createCharacter', function(data)
+AddEventHandler('qb-multicharacter:server:createCharacter', function(data, enabledhouses)
     local src = source
     local newData = {}
     newData.cid = data.cid
     newData.charinfo = data
-    --QBCore.Player.CreateCharacter(src, data)
     if QBCore.Player.Login(src, false, newData) then
         print('^2[qb-core]^7 '..GetPlayerName(src)..' has succesfully loaded!')
         QBCore.Commands.Refresh(src)
-        loadHouseData()
+        --[[if enabledhouses then
+            loadHouseData()
+        end]]
 
         TriggerClientEvent("qb-multicharacter:client:closeNUI", src)
-        TriggerClientEvent('apartments:client:setupSpawnUI', src, newData)
-        GiveStarterItems(src)
+        TriggerClientEvent('qb-spawn:client:setupSpawnUI', src, newData, true)
+        --GiveStarterItems(src)
 	end
 end)
-
-function GiveStarterItems(source)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-
-    for k, v in pairs(QBCore.Shared.StarterItems) do
-        local info = {}
-        if v.item == "id_card" then
-            info.citizenid = Player.PlayerData.citizenid
-            info.firstname = Player.PlayerData.charinfo.firstname
-            info.lastname = Player.PlayerData.charinfo.lastname
-            info.birthdate = Player.PlayerData.charinfo.birthdate
-            info.gender = Player.PlayerData.charinfo.gender
-            info.nationality = Player.PlayerData.charinfo.nationality
-        elseif v.item == "driver_license" then
-            info.firstname = Player.PlayerData.charinfo.firstname
-            info.lastname = Player.PlayerData.charinfo.lastname
-            info.birthdate = Player.PlayerData.charinfo.birthdate
-            info.type = "A1-A2-A | AM-B | C1-C-CE"
-        end
-        Player.Functions.AddItem(v.item, 1, false, info)
-    end
-end
 
 RegisterServerEvent('qb-multicharacter:server:deleteCharacter')
 AddEventHandler('qb-multicharacter:server:deleteCharacter', function(citizenid)
@@ -67,21 +42,7 @@ AddEventHandler('qb-multicharacter:server:deleteCharacter', function(citizenid)
     QBCore.Player.DeleteCharacter(src, citizenid)
 end)
 
-QBCore.Functions.CreateCallback("qb-multicharacter:server:GetUserCharacters", function(source, cb)
-    local license = QBCore.Functions.GetIdentifier(source, 'license')
-
-    exports['ghmattimysql']:execute('SELECT * FROM players WHERE license=@license', {['@license'] = license}, function(result)
-        cb(result)
-    end)
-end)
-
-QBCore.Functions.CreateCallback("qb-multicharacter:server:GetServerLogs", function(source, cb)
-    exports['ghmattimysql']:execute('SELECT * FROM server_logs', function(result)
-        cb(result)
-    end)
-end)
-
-QBCore.Functions.CreateCallback("test:yeet", function(source, cb)
+QBCore.Functions.CreateCallback("qb-multicharacter:server:loadUserInfo", function(source, cb)
     local license = QBCore.Functions.GetIdentifier(source, 'license')
     local plyChars = {}
     
@@ -97,25 +58,14 @@ QBCore.Functions.CreateCallback("test:yeet", function(source, cb)
     end)
 end)
 
-QBCore.Commands.Add("logout", "Logout of Character (Admin Only)", {{name="id", help="Player ID"},{name="item", help="Name of the item (not a label)"}, {name="amount", help="Amount of items"}}, false, function(source, args)
-    QBCore.Player.Logout(source)
-    TriggerClientEvent('qb-multicharacter:client:chooseChar', source)
-end, "admin")
-
-QBCore.Commands.Add("closeNUI", "Close Multi NUI", {{name="id", help="Player ID"},{name="item", help="Name of the item (not a label)"}, {name="amount", help="Amount of items"}}, false, function(source, args)
-    TriggerClientEvent('qb-multicharacter:client:closeNUI', source)
-end)
-
-QBCore.Functions.CreateCallback("qb-multicharacter:server:getSkin", function(source, cb, cid)
+function GiveStarterItems(source)
     local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
 
-    local result = exports.ghmattimysql:executeSync('SELECT * FROM playerskins WHERE citizenid=@citizenid AND active=@active', {['@citizenid'] = cid, ['@active'] = 1})
-    if result[1] ~= nil then
-        cb(result[1].model, result[1].skin)
-    else
-        cb(nil)
+    for k, v in pairs(QBCore.Shared.StarterItems) do
+        Player.Functions.AddItem(v.item, 1)
     end
-end)
+end
 
 function loadHouseData()
     local HouseGarages = {}
