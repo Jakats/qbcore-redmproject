215108d1e4b6e5a5e87259526e9fb1cdb8e38c52
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-08-11 13:55:10 +0200

    Houses and Apartments config

--- qb-spawn/config.lua
+++ qb-spawn/config.lua
@@ -1,5 +1,11 @@
 QB = {}
 
+-- Enable spawning inside houses from the spawn selector
+QB.EnableHouses = false
+
+-- Enable spawning inside apartments from the spawn selector
+QB.EnableApartments = false
+
 QB.Spawns = {
     ["emerald"] = {
         coords = {
@@ -21,5 +27,18 @@ QB.Spawns = {
         },
         location = "rhodesst",
         label = "Rhodes Sheriff Station",
+    }
+}
+
+QB.DefaultSpawn = {
+    ["default"] = {
+        coords = {
+            x = 1359.575, 
+            y = -1301.451, 
+            z = 77.76775, 
+            h = 3.5 
+        },
+        location = "default",
+        label = "Default Spawn"
     }
 }
